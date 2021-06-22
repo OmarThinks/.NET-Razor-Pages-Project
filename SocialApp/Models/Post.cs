b355d913cf736470c98f69830b1982099733aa1a
Omar Magdy <contact.parmag@gmail.com>
2021-06-22 14:06:01 +0200

    Post model: updated release date display name

--- SocialApp/Models/Post.cs
+++ SocialApp/Models/Post.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesMovie.Models
 {
@@ -9,6 +10,7 @@ namespace RazorPagesMovie.Models
         public string Content { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
     }
 }
