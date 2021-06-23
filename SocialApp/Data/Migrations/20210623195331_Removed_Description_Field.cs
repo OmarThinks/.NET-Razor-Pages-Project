63904a91ddb488dfbe29cd0ae6d52fba627b87ef
Omar Magdy <contact.parmag@gmail.com>
2021-06-23 22:52:59 +0200

    added a useless migration

---
+++ SocialApp/Data/Migrations/20210623195331_Removed_Description_Field.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SocialApp.Data.Migrations
+{
+    public partial class Removed_Description_Field : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
