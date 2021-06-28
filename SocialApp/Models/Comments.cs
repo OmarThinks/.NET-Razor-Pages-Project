fe54602c09f23cf9845bcafc5fc93ee24c04ef86
Omar Magdy <contact.parmag@gmail.com>
2021-06-28 20:15:07 +0200

    Models:add:comments model

---
+++ SocialApp/Models/Comments.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPagesMovie.Models
+{
+    public class Comment
+    {
+        public int ID { get; set; }
+        public string Content { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
