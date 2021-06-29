94783e1508eef2c48a19f036b3d1459aa7103a7e
Omar Magdy <contact.parmag@gmail.com>
2021-06-29 15:29:36 +0200

    migrated the commnet model

---
+++ SocialApp/Data/Migrations/20210629132856_CreatingTheCommentModel.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SocialApp.Data.Migrations
+{
+    public partial class CreatingTheCommentModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PostId",
+                table: "Comment",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_PostId",
+                table: "Comment",
+                column: "PostId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comment_Post_PostId",
+                table: "Comment",
+                column: "PostId",
+                principalTable: "Post",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comment_Post_PostId",
+                table: "Comment");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comment_PostId",
+                table: "Comment");
+
+            migrationBuilder.DropColumn(
+                name: "PostId",
+                table: "Comment");
+        }
+    }
+}
